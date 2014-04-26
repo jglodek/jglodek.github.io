57b5e8b7e9c7039e2b2744d85e67c3021f66c235
Jacek Głodek <jacek.glodek@gmail.com>
2014-04-26 14:54:44 +0200

    Automated commit at 2014-04-26 12:54:44 UTC by middleman-deploy 0.2.3

--- javascripts/all.js
+++ javascripts/all.js
@@ -32,9 +32,8 @@ window.matchMedia=window.matchMedia||(function(e,f){var c,a=e.documentElement,b=
       windowHeight = $(window).height();
       ratio = scrollTop / windowHeight;
       translate3d = "translate3d(0px," + (ratio * 300) + "px, 0px)";
-      heroBottom.css("-webkit-transform", translate3d);
       heroBottom.css("transform", translate3d);
-      return heroBottom.css("opacity", 1 - 1 * ratio);
+      return heroBottom.css("opacity", 1 - 1.3 * ratio);
     };
     $(window).resize(function() {
       return refreshHeroHeight();
@@ -42,14 +41,21 @@ window.matchMedia=window.matchMedia||(function(e,f){var c,a=e.documentElement,b=
     refreshHeroHeight();
     i = 0;
     $(window).scroll(function() {
-      if (i % 2 === 0) {
+      if (i % 1 === 0) {
         refreshHeroBottomPosition();
         i = 0;
       }
       return i += 1;
     });
+    $(".hero-bottom").css({
+      "margin-bottom": "50px",
+      opacity: 0
+    });
     initialCallback = function() {
-      return $(".hero").removeClass("initial");
+      return $(".hero-bottom").animate({
+        "margin-bottom": "0",
+        opacity: 1
+      }, 1000);
     };
     return setTimeout(initialCallback, 100);
   });
